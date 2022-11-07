ca2bb78a1ae7fd83c9bc937c128eaf5f8ffcb08b
Marina Paskal <paskalm@rogers.com>
2022-11-06 21:34:29 -0500

    Add pages

--- my-blog/src/App.js
+++ my-blog/src/App.js
@@ -1,10 +1,21 @@
 import "./App.css";
+import AboutPage from "./pages/AboutPage";
+import ArticlePage from "./pages/ArticlePage";
+import ArticlesListPage from "./pages/ArticlesListPage";
+import HomePage from "./pages/HomePage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 function App() {
   return (
     <div className="App">
       <h1>My Dev Blog</h1>
-      <div id="page-body">Welcome to my blog</div>
+      <div id="page-body">
+        <HomePage />
+        <AboutPage />
+        <ArticlesListPage />
+        <ArticlePage />
+        <NotFoundPage />
+      </div>
     </div>
   );
 }
---
+++ my-blog/src/pages/ArticlesListPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ArticlesListPage = () => {
+  return <h1>Articles Page</h1>;
+};
+
+export default ArticlesListPage;
