44027c091917ddea453e61a52c31a762e1c3d643
Marina Paskal <paskalm@rogers.com>
2022-11-15 00:32:53 -0500

    Add navbar

--- my-blog/src/App.js
+++ my-blog/src/App.js
@@ -1,4 +1,6 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NavBar from "./NavBar";
 import AboutPage from "./pages/AboutPage";
 import ArticlePage from "./pages/ArticlePage";
 import ArticlesListPage from "./pages/ArticlesListPage";
@@ -7,16 +9,19 @@ import NotFoundPage from "./pages/NotFoundPage";
 
 function App() {
   return (
-    <div className="App">
-      <h1>My Dev Blog</h1>
-      <div id="page-body">
-        <HomePage />
-        <AboutPage />
-        <ArticlesListPage />
-        <ArticlePage />
-        <NotFoundPage />
+    <BrowserRouter>
+      <div className="App">
+        <NavBar />
+        <div id="page-body">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/articles" element={<ArticlesListPage />} />
+            <Route path="/articles/:articleId" element={<ArticlePage />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
