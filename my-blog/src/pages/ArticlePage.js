7ebe05d15dbac1af3dbcc69572eac511d4bddc61
Marina Paskal <paskalm@rogers.com>
2022-11-22 13:31:46 -0500

    Add articles

--- my-blog/src/pages/ArticlePage.js
+++ my-blog/src/pages/ArticlePage.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import articles from "./article-content";
+import NotFoundPage from "./NotFoundPage";
 
 const ArticlePage = () => {
-  return <h1>Article Page</h1>;
+  const { articleId } = useParams();
+  const article = articles.find((article) => article.name === articleId);
+  if (!article) {
+    return <NotFoundPage />;
+  }
+
+  return (
+    <>
+      <h1>{article.title}</h1>
+      {article.content.map((paragraph, i) => (
+        <p key={i}>{paragraph}</p>
+      ))}
+    </>
+  );
 };
 
 export default ArticlePage;
--- my-blog/src/pages/NotFoundPage.js
+++ my-blog/src/pages/NotFoundPage.js
@@ -1,7 +1,5 @@
 import React from "react";
 
-const NotFoundPage = () => {
-  return <h1>Not Found Page</h1>;
-};
+const NotFoundPage = () => <h1>404: Page Not Found!</h1>;
 
 export default NotFoundPage;
